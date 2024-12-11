7562ba7338925552325bee5c86b77d62f8efb6cd
s-sanjoria94 <shubhamsanjoria@gmail.com>
2024-12-11 16:35:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Learning DevOps with Python v3"
+    return "Learning DevOps with Python v1"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
